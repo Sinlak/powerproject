4596d39bd73ffc430b98dd4d19753628524543d1
Earl <drolarumil@gmail.com>
2015-05-01 18:44:20 -0700

    turns out I need to make the login page first before bothering with the set lifts, going to do so now

--- app/controllers/set_lifts_controller.rb
+++ app/controllers/set_lifts_controller.rb
@@ -1,2 +1,28 @@
 class SetLiftsController < ApplicationController
+
+  def index
+  end
+
+  def new
+    @lifts = SetLifts.new
+  end
+
+  def create
+    @lifts = SetLifts.new(sign_ups_params)
+
+    if @lifts.save
+
+      flash[:notice] = "Thank you " + @user.first_name + ", lifts set successfully."
+      redirect_to(:action => '../home')
+
+    end
+
+
+  end
+
+  private
+    def sign_ups_params
+      params.require(:set_lifts).permit(:Squat, :Bench, :Dead, :OHP)
+    end
+
 end
