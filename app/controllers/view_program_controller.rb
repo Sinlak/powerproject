ccaba305d2aacff21a9dd348f2e63e270005cec9
earl <drolarumil@gmail.com>
2015-05-14 19:59:03 -0700

    Trying to clean it up

--- app/controllers/view_program_controller.rb
+++ app/controllers/view_program_controller.rb
@@ -1,6 +1,5 @@
 class ViewProgramController < ApplicationController
   before_action :confirm_logged_in, :except => [:login, :attempt_login, :logout]
-  before_action :check_lifts
   def index
   end
 end
