25686f428be30d348b2e537d9e15d047bc6e0580
earl <drolarumil@gmail.com>
2015-05-18 17:56:52 -0700

    fuck

--- app/controllers/sign_ups_controller.rb
+++ app/controllers/sign_ups_controller.rb
@@ -1,7 +1,7 @@
 class SignUpsController < ApplicationController
 
 
-  def index    
+  def index
   end
 
   def new
@@ -25,9 +25,9 @@ class SignUpsController < ApplicationController
       flash[:error] = "something failed"
       redirect_to :back
     end
-  rescue
-    flash[:error] = "Failed to create user, email is taken or passwords do not match"
-    redirect_to :back
+  # rescue
+  #   flash[:error] = "Failed to create user, email is taken or passwords do not match"
+  #   redirect_to :back
    end
 
   private
